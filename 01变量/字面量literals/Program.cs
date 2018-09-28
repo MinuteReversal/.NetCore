3d4ec8908a1f6e7f3c97dff7fa537bfa97ea33e8
MinuteReversal <114233763@qq.com>
2018-09-28 11:34:21 +0800

    literals

--- 01变量/字面量literals/Program.cs
+++ 01变量/字面量literals/Program.cs
@@ -4,9 +4,30 @@ namespace app
 {
     class Program
     {
+        enum Day { Sat, Sun, Mon, Tue, Wed, Thu, Fri };
+        public struct Book
+        {
+            public decimal price;
+            public string title;
+            public string author;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //字面量 https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/byte
+            bool b = true;
+            byte bt1 = 0b0000_00001;
+            int i1 = 0xfffff;
+            float f1 = 0.1f;
+            decimal d1 = 0.2222m;
+            char c1 = 'X';
+            char c2 = '\x0058';
+            char c3 = '\u0058';
+            double d = 3d;
+            float f = 1.01f;
+            long l = 4294967296;
+            sbyte sbt = sbyte.MinValue;
+
         }
     }
 }

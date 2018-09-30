6abd9f37cecabb61736cf91bfaf8a97a10da6f28
MinuteReversal <114233763@qq.com>
2018-09-30 17:27:50 +0800

    event

--- 07事件/app/Program.cs
+++ 07事件/app/Program.cs
@@ -2,27 +2,30 @@
 
 namespace app
 {
+    #region  广播站    
     class BroadcastingStation
     {
         public event EventHandler Broadcast;
         public void play()
         {
+            //派发内容
             Broadcast(this, new BroadcastContent { Message = "10月1日国庆放假7天" });
         }
     }
 
+    //广播的内容
     class BroadcastContent : EventArgs
     {
         public string Message;
     }
-
+    #endregion
     class Program
     {
         static void Main(string[] args)
         {
             var bs = new BroadcastingStation();
-            bs.Broadcast += Listener1;            
-            bs.Broadcast += Listener2;
+            bs.Broadcast += Listener1;//订阅
+            bs.Broadcast += Listener2;//订阅
             bs.play();
         }
 
@@ -35,7 +38,7 @@ namespace app
         }
 
         /*
-        * 听众1
+        * 听众2
         */
         static void Listener2(object sender, EventArgs e)
         {

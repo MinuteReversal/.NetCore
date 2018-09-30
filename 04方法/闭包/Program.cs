062be77a8a088d2aeeb513eb324dcf98e4f3cdac
MinuteReversal <114233763@qq.com>
2018-09-30 15:40:37 +0800

    close bag

---
+++ 04方法/闭包/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            Action f1 = () =>
+            {
+                var i2 = 2;
+                Action f2 = () =>
+                {
+                    var i3 = 3;
+                    Console.WriteLine($"{i}{i2}{i3}");
+                };
+                f2();
+            };
+            
+            f1();
+        }
+    }
+}

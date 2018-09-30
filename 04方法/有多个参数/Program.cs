4aa7b27010138e2d926645fe7419750a027a1b20
MinuteReversal <114233763@qq.com>
2018-09-30 14:12:45 +0800

    params

---
+++ 04方法/有多个参数/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManyParams("a", "b", "c");
+        }
+
+        static void ManyParams(params string[] args)
+        {
+            foreach (var a in args)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}

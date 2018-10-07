9acf8d6970c805acb376f2896b372a120e23de19
MinuteReversal <114233763@qq.com>
2018-10-07 21:23:47 +0800

    common variable maxvalue and minvalue

---
+++ 01变量/变量的最大值与最小值/app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iMax = int.MaxValue;
+            int iMin = int.MinValue;
+            float fMin = float.MaxValue;
+            float fMax = float.MinValue;
+            double dMax = double.MaxValue;
+            double dMin = double.MinValue;
+            byte bMax = byte.MaxValue;
+            byte bMin = byte.MinValue;
+            bool bYes = true;
+            bool bNo = false;
+        }
+    }
+}

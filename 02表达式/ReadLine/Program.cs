122cc9b05f7ae8c87e4f192a22655b65e1ab5148
MinuteReversal <114233763@qq.com>
2018-10-07 21:35:48 +0800

    ReadLine

---
+++ 02表达式/ReadLine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input some words");
+            string someInput = Console.ReadLine();
+            Console.WriteLine("your input is {0}", someInput);
+        }
+    }
+}

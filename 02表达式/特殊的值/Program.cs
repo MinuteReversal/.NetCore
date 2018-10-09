545e2d296d7970aeff151dc322ab0e70a620ce99
MinuteReversal <114233763@qq.com>
2018-10-09 14:13:35 +0800

    special value

---
+++ 02表达式/特殊的值/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace 特殊的值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("" == null);
+            Console.WriteLine(0 == null);
+            Console.WriteLine(false == null);
+        }
+    }
+}

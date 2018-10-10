1a4d3dbdd82bfdd8428be8ce178198726874472e
MinuteReversal <114233763@qq.com>
2018-10-10 09:08:11 +0800

    order

---
+++ 03流程控制/01顺序结构/01顺序的重要性/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01顺序的重要性
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 6;
+            i = i * 2;
+            i = i + 2;
+            Console.WriteLine("i 的值是 {0}", i);
+        }
+    }
+}

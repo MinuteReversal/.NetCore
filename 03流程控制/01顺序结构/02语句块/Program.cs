e43632c2830fb747c4a114b1546183e336575c9c
MinuteReversal <114233763@qq.com>
2018-10-10 09:15:40 +0800

    sentence block

---
+++ 03流程控制/01顺序结构/02语句块/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02语句块
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int i = 1;
+                i += 2;
+                i *= 2;
+                Console.WriteLine("the value of i is {0}", i);
+            }
+        }
+    }
+}

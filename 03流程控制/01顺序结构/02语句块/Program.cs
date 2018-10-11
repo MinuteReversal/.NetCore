1fb69567b36eb0cd898c2a2851df8a8932aa9be1
MinuteReversal <114233763@qq.com>
2018-10-11 23:54:23 +0800

    commit

--- 03流程控制/01顺序结构/02语句块/Program.cs
+++ 03流程控制/01顺序结构/02语句块/Program.cs
@@ -6,12 +6,14 @@ namespace _02语句块
     {
         static void Main(string[] args)
         {
+            int a = 123;
             {
                 int i = 1;
                 i += 2;
                 i *= 2;
-                Console.WriteLine("the value of i is {0}", i);
+                Console.WriteLine("the value of i is {0}{1}", i, a);
             }
+            Console.WriteLine(i);
         }
     }
 }
---
+++ 03流程控制/02选择结构/01if语句/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01if语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 32;
+            if (age >= 33)
+            {
+                Console.WriteLine("我是zhy");
+                Console.WriteLine("我今年33岁");
+            }
+            Console.WriteLine("大家好");
+        }
+    }
+}

c611f23381dbac24a4a23ad93eb0cf5fd804f110
MinuteReversal <114233763@qq.com>
2018-10-15 23:07:20 +0800

    test

---
+++ 03流程控制/02选择结构/实验02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 实验02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = "a==10";
+            switch (number)
+            {
+                case "a==10":
+                    Console.WriteLine("a的值是10");
+                    break;
+                case "a==20":
+                    Console.WriteLine("a的值是20");
+                    break;
+                case "a==30":
+                    Console.WriteLine("a的值是30");
+                    break;
+                default:
+                    Console.WriteLine("没有匹配项");
+                    break;
+            }
+            Console.WriteLine("a的准确值是{0}", number);
+        }
+    }
+}

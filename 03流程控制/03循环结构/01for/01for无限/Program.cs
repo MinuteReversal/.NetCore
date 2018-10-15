ababaeb01dadeea62d7d30c291b2883d4d7ed62e
MinuteReversal <114233763@qq.com>
2018-10-15 15:55:29 +0800

    loop

---
+++ 03流程控制/03循环结构/01for/01for无限/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01for无限
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; ) Console.WriteLine("hello world");
+        }
+    }
+}
---
+++ 03流程控制/03循环结构/02while/01while无限/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01while无限
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) Console.WriteLine("Hello World!");
+        }
+    }
+}

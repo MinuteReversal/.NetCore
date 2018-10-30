cf86f8be3c892138746b3a2e93f2635a70076cae
MinutesReversal <114233763@qq.com>
2018-10-30 22:44:17 +0800

    char test

---
+++ 05字字符串集合枚举结构体/01字符与ASCII/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01字符与ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1 = (char)97;
+            char c2 = (char)65;
+            int i1 = (int)c1;
+            int i2 = (int)c2;
+            Console.WriteLine($"{c1}{c2}{i1}{i2}");
+        }
+    }
+}

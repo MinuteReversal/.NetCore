cbb70c30269e2522a4a0a0f6a314ea5481fea2b0
MinutesReversal <114233763@qq.com>
2018-10-30 23:40:01 +0800

    ascii

--- 05字字符串集合枚举结构体/01字符与ASCII/Program.cs
+++ 05字字符串集合枚举结构体/01字符与ASCII/Program.cs
@@ -6,6 +6,13 @@ namespace _01字符与ASCII
     {
         static void Main(string[] args)
         {
+            char cx1 = 'X';
+            char cx2 = '\x0058';
+            char cx3 = (char)88;
+            char cx4 = '\u0058';
+
+            Console.WriteLine($"{cx1},{cx2},{cx3},{cx4}");
+
             char c1 = (char)97;
             char c2 = (char)65;
             int i1 = (int)c1;

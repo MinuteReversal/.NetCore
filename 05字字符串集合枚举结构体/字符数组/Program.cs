a9802dd19a02b347af9892d8effad7be2b6fc533
MinutesReversal <114233763@qq.com>
2018-10-30 23:20:45 +0800

    char to array

---
+++ 05字字符串集合枚举结构体/字符数组/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 字符数组
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] cs1 = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd' };
+            Console.WriteLine(cs1);
+
+            string s1 = new string(cs1);
+            Console.WriteLine(s1);
+
+        }
+    }
+}

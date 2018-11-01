f9e9db440af4afe033b929d9ba27b2ddc5e913b9
MinutesReversal <114233763@qq.com>
2018-11-01 10:29:44 +0800

    struct

--- 05字字符串集合枚举结构体/结构体/Program.cs
+++ 05字字符串集合枚举结构体/结构体/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace 结构体
 {
@@ -7,6 +8,7 @@ namespace 结构体
     {
         struct Student
         {
+
             public string name;
 
             public int age;
@@ -17,28 +19,39 @@ namespace 结构体
 
         static void Main(string[] args)
         {
-            Student s;
-            s.age = 31;
-            s.name = "lily";
-            s.gender = 0;
+            // Student s;
+            // Console.WriteLine(s1.age);
+            // Console.WriteLine(s1.name);
+            // Console.WriteLine(s1.gender);
 
-            Student s2 = new Student();
+            //===============需要对每个成员赋值=================
+            Student s1;
+            s1.age = 31;
+            s1.name = "lily";
+            s1.gender = 0;
+
+            Student s2;
             s2.age = 18;
             s2.name = "lucy";
             s2.gender = 0;
 
-            Student s3 = new Student();
+            Student s3;
             s3.age = 22;
-            s3.name = "li lei";
+            s3.name = "lilei";
             s3.gender = 1;
 
-            Console.WriteLine(s.name);
+            Console.WriteLine(s1.name);
 
-            Student[] ss = { s, s2, s3 };
+            Student[] ss = { s1, s2, s3 };
             foreach (var student in ss)
             {
                 Console.WriteLine(student.name);
             }
+
+            List<Student> ss1 = new List<Student>();
+            ss1.Add(s1);
+            ss1.Add(s2);
+            ss1.Add(s3);
         }
     }
 }

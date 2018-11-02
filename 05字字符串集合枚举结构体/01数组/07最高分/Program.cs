bd21a0de3256a4d9476c112e23bea67483cc941d
MinutesReversal <114233763@qq.com>
2018-11-02 11:12:58 +0800

    average

---
+++ 05字字符串集合枚举结构体/01数组/07最高分/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _07最高分
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 61, 73, 84, 66, 99, 100 };
+            int max = 0;
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (max < a[i]) max = a[i];
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ 05字字符串集合枚举结构体/01数组/08平均分/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08平均分
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 61, 73, 84, 66, 99, 100 };
+            int sum = 0;
+            for (var i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine(sum / a.Length);
+        }
+    }
+}

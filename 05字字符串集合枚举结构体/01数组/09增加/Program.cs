0bbc6bfb17b6e656c2c9d1090e31bf5037ab7eba
MinutesReversal <114233763@qq.com>
2018-11-02 11:33:03 +0800

    remove last

---
+++ 05字字符串集合枚举结构体/01数组/09增加/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09增加
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 61, 73, 84, 66, 99, 100 };
+            int[] a1 = new int[a.Length + 1];
+            for (var i = 0; i < a.Length; i++)
+            {
+                a1[i] = a[i];
+            }
+            a1[a1.Length - 1] = 88;
+            Console.WriteLine(string.Join(",", a1));
+        }
+    }
+}
---
+++ 05字字符串集合枚举结构体/01数组/10删除/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10删除
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 61, 73, 84, 66, 99, 100, 88 };
+            int[] a1 = new int[a.Length - 1];
+            for (var i = 0; i < a1.Length; i++)
+            {
+                a1[i] = a[i];
+            }
+            Console.WriteLine(string.Join(",", a1));
+        }
+    }
+}

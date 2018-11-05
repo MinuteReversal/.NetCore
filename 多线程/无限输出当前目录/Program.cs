3cb8acda42bb6c1d91664f21b5546df0def1218c
MinutesReversal <114233763@qq.com>
2018-11-05 14:17:41 +0800

    output cmd

---
+++ 多线程/无限输出当前目录/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace 无限输出当前目录
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = string.Empty;
+            string error = string.Empty;
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c dir");
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            processStartInfo.UseShellExecute = false;
+
+            Process process = Process.Start(processStartInfo);
+            using (StreamReader streamReader = process.StandardOutput)
+            {
+                output = streamReader.ReadToEnd();
+            }
+            //无限输出
+            for (; ; ) Console.WriteLine(output);
+        }
+    }
+}

33dcbf660ca6dee5fdfab2156e3b36ed0805435a
MinutesReversal <114233763@qq.com>
2018-11-08 16:16:14 +0800

    unicode

--- 试验田/Draw/DrawUnicode/Program.cs
+++ 试验田/Draw/DrawUnicode/Program.cs
@@ -6,11 +6,14 @@ namespace DrawUnicode
     {
         static void Main(string[] args)
         {
+            //https://en.wikipedia.org/wiki/List_of_Unicode_characters
+            //https://unicode-table.com/en/#miscellaneous-symbols
+
             Console.WriteLine(Console.OutputEncoding);
             Console.OutputEncoding = System.Text.Encoding.UTF8;//非常重要
 
             char[] rList = new char[] { (char)9617, (char)9618, (char)9619, (char)9608 };
-            char[] uList = { '\u2590', '\u2591', '\u2592', '\u2593' };
+            char[] uList = { '\u2588', '\u2591', '\u2592', '\u2593' };
             char[] bList = { '\u25A4', '\u25A5', '\u25A6', '\u25A7', '\u25A8', '\u25A9' };
             char[] buttons = { '\u23E9', '\u23EA', '\u23EB', '\u23EC', '\u23ED', '\u23EE', '\u23EF', '\u23F5', '\u23F6', '\u23F7', '\u23F8', '\u23F9', '\u23FA' };
             char[] palyCard = { '\u2660', '\u2661', '\u2662', '\u2663', '\u2664', '\u2665', '\u2666', '\u2667' };
---
+++ 试验田/Draw/UnicodeTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UnicodeTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.UnicodeEncoding.UTF8;
+            Console.Write((char)0 == '\u0000');
+            for (var i = -1; i <= 0xFFFF / 6; i += 10)
+            {
+                for (var j = 0; j <= 0xF; j++)
+                {
+                    Console.Write("\u0020");
+                    Console.Write((char)(i + j));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

45caa812a568ec96f1ee404250d035b6540263db
MinutesReversal <114233763@qq.com>
2018-11-08 16:24:56 +0800

    find code

---
+++ 试验田/Draw/UTF8Find/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UTF8Find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "";
+            do
+            {
+                Console.WriteLine("input word");
+                input = Console.ReadLine();
+                Console.WriteLine($"{input}:{GetCode(input[0]).ToString("X")}");
+
+            } while (input != "\u001B");
+        }
+
+        static int GetCode(char c)
+        {
+            for (var i = 0; i < 0xFFFF; i++)
+            {
+                if ((char)i == c)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}

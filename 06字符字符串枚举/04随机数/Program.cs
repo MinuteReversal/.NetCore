9490a29736fd08a3d1a2c51fa31d3d7f22892e61
MinutesReversal <114233763@qq.com>
2018-11-09 21:37:46 +0800

    random

---
+++ 06字符字符串枚举/04随机数/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04随机数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(r.Next(0, 100));
+            }
+        }
+    }
+}

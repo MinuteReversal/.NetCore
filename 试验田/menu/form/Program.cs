6b8cf233002a9334cd80a5c4871b7c5a35fe96ec
MinutesReversal <114233763@qq.com>
2018-11-13 22:18:50 +0800

    unicode

---
+++ 试验田/menu/form/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //http://www.fileformat.info/info/unicode/block/geometric_shapes/list.htm
+            //https://emojipedia.org/ballot-box-with-check/
+            //https://www.iemoji.com/
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("🔘  \u25EF ⚪️ \u25C9 ☑️ ✅ ✔️ ❌ ⭕ ❎  💾  🖫  🖮  🖰  🖳  🖴");
+            Console.WriteLine("\u25A2 \u25BA \u25B6 \u25C9 \u25EF");
+            Console.WriteLine("☘️  🍀");
+        }
+    }
+}

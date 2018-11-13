51e5fb4c43c2a2b54ecbb9cadd1ce95e473ae90c
MinutesReversal <114233763@qq.com>
2018-11-13 18:14:40 +0800

    menu

---
+++ 试验田/menu/01border/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+namespace menu
+{
+    class Program
+    {
+
+        //https://stackoverflow.com/questions/20999247/how-to-prevent-the-console-to-write-a-character-in-the-last-line
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+            MenuBox();
+        }
+
+        static void MenuBox()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (var y = 0; y < Console.WindowHeight; y++)
+            {
+                for (var x = 0; x < Console.WindowWidth; x++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 试验田/mp3/01.mp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.mp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

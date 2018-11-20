6b2379bfced56689e63e35b8e80248c2f05bb976
MinutesReversal <114233763@qq.com>
2018-11-20 15:52:46 +0800

    move UTF8Find

---
+++ 试验田/UTF8Find/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace UTF8Find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            var input = "";
+            do
+            {
+                Console.WriteLine("input word");
+                input = Console.ReadLine();
+                Console.WriteLine($"{input}:{GetCode(input[0]).ToString("X")}");
+
+            } while (input != "\u001B");
+        }
+
+        static int GetCode(char c)
+        {
+            for (var i = 0; i < 0xFFFF; i++)
+            {
+                if ((char)i == c)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}

637ee7f85fbb2b485ce3930d632450e138889c44
MinutesReversal <114233763@qq.com>
2018-11-21 21:09:21 +0800

    sort accessor

---
+++ 07对象与类/06简化属性/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06简化属性
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student[] soft1801 = new Student[]{
+                new Student{Name="lily",Gender=0,Birthday=new DateTime(1985,1,1)},
+                new Student{Name="lucy",Gender=0,Birthday=new DateTime(1985,1,1)},
+                new Student{Name="lilei",Gender=0,Birthday=new DateTime(1985,1,1)}
+            };
+
+        }
+    }
+}

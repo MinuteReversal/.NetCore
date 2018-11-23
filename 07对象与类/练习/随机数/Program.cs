97c403a85deffc5d6277f023c103a4ebf52780a5
MinutesReversal <114233763@qq.com>
2018-11-23 18:07:04 +0800

    process

---
+++ 07对象与类/练习/随机数/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace 随机数
+{
+    class Program
+    {
+        static void RollNumberToChar(int Number)
+        {
+            char[] dice = new char[] { '\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685' };
+            Console.WriteLine($"{dice[Number]}:{Number + 1}");
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;            
+            string input = "";
+            while (input != "q")
+            {
+                Console.Write("来投个骰子\n按任意按键投。\n");
+                input = Console.ReadLine();
+                RollNumberToChar(random.Next(0, 6));
+            }
+        }
+    }
+}

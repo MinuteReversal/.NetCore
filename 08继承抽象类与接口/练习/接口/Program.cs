7eac91302f69a770cf7ba3395fd8a58fdaff4a87
MinutesReversal <114233763@qq.com>
2018-12-03 14:19:46 +0800

    interface

---
+++ 08继承抽象类与接口/练习/接口/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace 接口
+{
+    //接口无法被new
+    interface IFather
+    {
+        void Coding();//会写程序
+    }
+
+    interface IMother
+    {
+        void PlayPiano();//会弹钢琴
+    }
+
+    class Child : IFather, IMother
+    {
+        public string Name { get; set; }
+        public void Coding()
+        {
+            Console.WriteLine("孩子实现了父亲会写程序的梦想");
+        }
+
+        public void PlayPiano()
+        {
+            Console.WriteLine("孩子实现了母亲会弹古筝的梦想");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.Coding();
+            child.PlayPiano();
+        }
+    }
+}

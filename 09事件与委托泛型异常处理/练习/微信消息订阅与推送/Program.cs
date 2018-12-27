c69ab60b2d266ea30ef5d8d0fa5777500767e7a0
MinutesReversal <114233763@qq.com>
2018-12-27 12:37:52 +0800

    draw a face

---
+++ 09事件与委托泛型异常处理/练习/微信消息订阅与推送/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace 微信消息订阅与推送
+{
+    //微信
+    class Wechat
+    {
+        public event EventHandler Subscribe;
+        public void Send()
+        {
+            //派发内容
+            Subscribe(this, new MessageContent { Message = "2019年元旦放假三天。" });
+        }
+    }
+
+    //消息内容    
+    class MessageContent : EventArgs
+    {
+        public string Message { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wx = new Wechat();
+            wx.Subscribe += SubscriberA;//订阅
+            wx.Subscribe += SubscriberB;//订阅            
+            wx.Subscribe += SubscriberC;//订阅   
+            wx.Send();//微信推送消息
+        }
+
+        //订户A
+        static void SubscriberA(object sender, EventArgs e)
+        {
+            Console.WriteLine("订户A收到：" + (e as MessageContent).Message);
+        }
+
+        //订户B
+        static void SubscriberB(object sender, EventArgs e)
+        {
+            Console.WriteLine("订户B收到：" + (e as MessageContent).Message);
+        }
+
+        //订户C
+        static void SubscriberC(object sender, EventArgs e)
+        {
+            Console.WriteLine("订户C收到：" + (e as MessageContent).Message);
+        }
+    }
+}
---
+++ 11文件操作/练习/在图片上画一张笑脸/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+* 需要联网安装 System.Drawing.Common
+* ctrl+~ 终端里输入 dotnet add package System.Drawing.Common
+*                  ⬆      ⬆   ⬆
+*                小写    小写 小写
+*/
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace 在图片上画一张笑脸
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一张200像素X200像素的图片
+            Bitmap bitmap = new Bitmap(200, 200);
+
+            /* 用图像处理类加载这个图片 */
+            Graphics g = Graphics.FromImage(bitmap);
+
+            //创建一个笔,颜色白色，宽度为2个像素
+            Pen pen = new Pen(Color.White, 2);
+
+            /* 创建一个矩形，左上角为位置为0，0，宽度200，高度200，正好和图片一样大小 */
+            Rectangle rectangle = new Rectangle(0, 0, 200, 200);//画脸的位置
+
+            //https://docs.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawarc?view=netframework-4.7.2
+            //画一个弧线（笔，位置，开始角度，范围）
+            g.DrawArc(pen, rectangle, 0, 360);//从 x 轴到弧线的起始点沿顺时针方向度量的角
+
+            //左边眼睛位置
+            Rectangle leftEye = new Rectangle(30, 40, 60, 60);
+            //画一个弧线（笔，位置，开始角度，范围）
+            g.DrawArc(pen, leftEye, 0, -180);
+
+            //右边眼睛位置
+            Rectangle rightEye = new Rectangle(110, 40, 60, 60);
+            //画一个弧线（笔，位置，开始角度，范围）
+            g.DrawArc(pen, rightEye, 0, -180);
+
+            Rectangle mouth = new Rectangle(60, 90, 80, 80);
+            //画一个弧线（笔，位置，开始角度，范围）
+            g.DrawArc(pen, mouth, 0, 180);
+
+            bitmap.Save("1.png", ImageFormat.Png);
+        }
+    }
+}

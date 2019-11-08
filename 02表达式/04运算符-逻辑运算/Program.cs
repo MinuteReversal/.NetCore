6f1e9be3225590ac8ade25ae8a1784fef10fdf32
MinutesReversal <114233763@qq.com>
2019-11-08 14:09:54 +0800

    add comment

--- 02表达式/04运算符-逻辑运算/Program.cs
+++ 02表达式/04运算符-逻辑运算/Program.cs
@@ -12,21 +12,21 @@ namespace app
 
             Console.WriteLine($"{n1},{n2}");
 
-            //与
+            //与 两边同为true才为true
             bool a1 = true && true;
             bool a2 = true && false;
             bool a3 = false && true;
             bool a4 = false && false;
             Console.WriteLine($"{a1},{a2},{a3},{a4}");
 
-            //或
+            //或:两边同时为false才为false
             bool o1 = true || true;
             bool o2 = false || true;
             bool o3 = true || false;
             bool o4 = false || false;
             Console.WriteLine($"{o1},{o2},{o3},{o4}");
 
-            //异或 两边相同为false,不同为true
+            //异或:两边相同为false,不同为true
             bool x1 = true ^ true;
             bool x2 = false ^ true;
             bool x3 = true ^ false;

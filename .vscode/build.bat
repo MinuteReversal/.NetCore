b84b3cb094d6f1cddcd59efb2c3a7e87e447d3d7
MinuteReversal <114233763@qq.com>
2020-07-16 08:50:19 +0800

    complier setting

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -1,3 +1,7 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
-csc /debug:full %1
+set fileName=%1
+set fileName=%fileName:.cs=.exe%
+echo %fileName%
+echo %1
+csc %1 -debug:full -out:%fileName%

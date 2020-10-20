dad7c695233971b5ec7811b9001772f56e3d5a02
MinuteReversal <114233763@qq.com>
2020-10-20 23:06:08 +0800

    program

--- 01变量/string字符串/Program.cs
+++ 01变量/string字符串/Program.cs
@@ -10,6 +10,10 @@ namespace string字符串
             Console.WriteLine((int)s[0]);//输出第一个成员十进制输出
             Console.WriteLine(((int)s[1]).ToString("x"));//输出第二个成员16进制输出
             Console.WriteLine((s[2]));//输出第三个成员
+
+            string s1 = "hello world";
+            string result = s1.Substring(6, 5);
+            Console.WriteLine(result);
         }
     }
 }
